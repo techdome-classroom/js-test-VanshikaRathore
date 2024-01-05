6b2b7566ebd57b4374722b96a50cb91524eb83d7
VanshikaRathore <96684855+VanshikaRathore@users.noreply.github.com>
2024-01-05 13:42:26 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,9 +3,27 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    let stack = [];
+    let brackets = {
+        ")" : "(",
+        "]" : "[",
+        "}" : "{"
 };
-
+    for(let i =0; i,s.length;s++){
+        let char = s[i];
+        
+        if(char in brackets){
+              if(stack.length === 0 || stack[stack.length -1] != brackets[char]){
+                return false;
+              }
+              stack.pop();
+        }else{
+            stack.push(char);
+        }
+    }
+    return stack.length === 0;
+};
+          
 module.exports = { isValid };
 
 
